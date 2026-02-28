a6fff3f8c07b983b4319fa0d16d320e1727de3c7
QuyNguyenHoang <proemie.2003@gmail.com>
2026-02-28 16:25:26 +0700

    no comment

--- ContentHub.Api/Program.cs
+++ ContentHub.Api/Program.cs
@@ -10,7 +10,6 @@ using ContentHub.Infrastructure.SeedWorks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
